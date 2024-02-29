4ea1e57c4d93f7666e663d0bafe7bfa485b31d2a
Masood-Hasan <mohdmasoodhasan@gmail.com>
2024-02-29 15:40:22 +0530

    The Final Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// import Typed from 'typed.js';
+function sign(){
+    alert("Currently site not updated!");
+}
+
+let typed= new Typed("#multiple-times",{
+    strings:[`FrontEnd Developer`,`Backend Developer`,`UI/UX Designer`],
+    typespeed:100,
+    backspeed:100,
+    backdelay:100,
+    loop:true,
+})
+
